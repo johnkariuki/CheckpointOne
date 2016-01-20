bc8579e00d40d10da2d466af1be760ce2d59d2c9
John Kariuki <john.kariuki@andel.comm>
2016-01-20 11:37:14 +0300

    Task 1: Add Urbanwords Class with an associative array of slang, meaning and desc

---
+++ src/UrbanWords.php
@@ -0,0 +1,23 @@
+<?php
+namespace John\Cp;
+
+class UrbanWords
+{
+	public static $data = array(
+		[
+            "slang" => "Tight",
+            "description" => "When someone performs an awesome task",
+            "sample-sentence" => "Prosper has finished the curriculum, Tight."
+        ],
+        [
+            "slang" => "lit",
+            "description" => "The state of being so intoxicated (regardless of the intoxicating agent) that all the person can do is smile, so that they look lit up like a light",
+            "sample-sentence" => "He's so lit he can't even talk."
+        ],
+        [
+            "slang" => "Turnt",
+            "description" => "State of being in full party mode.",
+            "sample-sentence" => "Yo were you at John's party? it was turnt!."
+        ]
+	);
+}
---
+++ tests/UrbanWordsTest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: johnkariuki
+ * Date: 20/01/2016
+ * Time: 10:06
+ */
+
+namespace John\Cp;
+use John\Cp\UrbanWords;
+
+class UrbanWordsTest extends \PHPUnit_Framework_TestCase
+{
+    public function testArray()
+    {
+        $this->assertTrue(is_array(UrbanWords::$data));
+    }
+
+    public function testArrayHasUrbanWords()
+    {
+        $this->assertNotEmpty(UrbanWords::$data);
+
+        $this->assertEquals('Tight', UrbanWords::$data[0]['slang']);
+        $this->assertEquals('When someone performs an awesome task', UrbanWords::$data[0]['description']);
+        $this->assertEquals('Prosper has finished the curriculum, Tight.', UrbanWords::$data[0]['sample-sentence']);
+    }
+
+    public  function testArrayHasKeys()
+    {
+        $keys = [
+            'slang',
+            'description',
+            'sample-sentence'
+        ];
+
+        $this->assertEmpty(array_diff($keys, array_keys(UrbanWords::$data[0])));
+    }
+}
